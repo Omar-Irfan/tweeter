d24d13624d90535cab4a74a49bbc315b0b5c992c
Omar Irfan <omar.irfan18@gmail.com>
2021-04-21 16:56:44 +0000

    implimented tweet creator functions in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,20 +3,32 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
   const createTweetElement = function (tweetObj) {
-    const username = tweetObj.user.name
+    const avatar = tweetObj.user.avatars
+    const name = tweetObj.user.name
+    const handle = tweetObj.user.handle
+    const content = tweetObj.content.text
 
-    const $tweet = $(`<div class = "name-header">
-    <div class = "user"action="/tweets" method="GET">name</div>
-    <div class = "handle">@name</div>
-  </div> 
-  <div class ="tweet">hifhsufoaihfiashf</div>
-  <footer class= "date-footer"> 
-    <div class = "date"><div class="timeago" datetime="2016-06-30 09:20:00"></div></div>`);
+    const tweet = $(`<article class="tweet-container">
+    <div class = "name-header">  
+      <div class = "user">
+        <img class = "avatar" src ="${avatar}"></img>
+        <div class = "username">${name}</div>
+      </div>
+      <div class = "handle">${handle}</div>
+    </div> 
+    <div class ="tweet">${content}</div>
+    <footer class= "date-footer"> 
+      <div class = "date"><div class="timeago" datetime="2016-06-30 09:20:00"></div></div>
+      <div class = "icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer> 
+  </article>`);
+  return tweet
   }
-   
-})
 
 
 const tweetData = {
@@ -30,3 +42,11 @@ const tweetData = {
     },
   "created_at": 1461116232227
 }
+
+const $tweet = createTweetElement(tweetData)
+
+
+$( document ).ready(function() {
+  $('.all-tweets').append($tweet); 
+  console.log($tweet);
+});
