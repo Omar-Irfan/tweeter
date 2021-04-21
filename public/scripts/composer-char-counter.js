574f9450f9f6d5921799d71dad70c1554204e82f
Omar Irfan <omar.irfan18@gmail.com>
2021-04-21 01:00:40 +0000

    added tweet container and css pertaining to it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   $("textarea").on('input', function() {
     const charaCount= 140 - $(this).val().length;
     const counter = $(this).parent().find('.counter')
-    console.log(counter[0].innerText = charaCount)
+    counter[0].innerText = charaCount
+    if (charaCount < 0){
+      counter.css("color", "red")
+    }
+    if (charaCount >= 0){
+      counter.css("color", "rgba(65, 60, 60, 0.836)")
+    }
+
   })
 });
