76ce3b204154ad69c2231dd4f6eef2e471095f0e
Omar Irfan <omar.irfan18@gmail.com>
2021-04-21 14:14:23 +0000

    added in avatar in tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,30 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const createTweetElement = function (tweetObj) {
+    const username = tweetObj.user.name
 
+    const $tweet = $(`<div class = "name-header">
+    <div class = "user"action="/tweets" method="GET">name</div>
+    <div class = "handle">@name</div>
+  </div> 
+  <div class ="tweet">hifhsufoaihfiashf</div>
+  <footer class= "date-footer"> 
+    <div class = "date"><div class="timeago" datetime="2016-06-30 09:20:00"></div></div>`);
+  }
+   
+})
+
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
