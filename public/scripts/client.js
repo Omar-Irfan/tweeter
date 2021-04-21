bf2bac7f36e524bf73001a5089d35548e8a1c096
Omar Irfan <omar.irfan18@gmail.com>
2021-04-21 19:58:51 +0000

    Implemented tweets loading from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,27 +1,3 @@
-const tweetData =  [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -62,13 +38,15 @@ const renderTweets = function(tweets) {
 };
 
 const loadTweets = function() {
-  
+  $.get("/tweets", function(tweets) {
+    renderTweets(tweets)
+  })
 }
 
 
 $(document).ready(function() {
-  renderTweets(tweetData);
-
+  loadTweets()
+  
   $(document).submit(function(event) {
     event.preventDefault()
   const submitData = $('#tweet-text').serialize()
