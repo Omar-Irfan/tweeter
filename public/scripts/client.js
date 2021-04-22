32f981fa996bf618e4947666e306a8c431de796a
Omar Irfan <omar.irfan18@gmail.com>
2021-04-22 23:30:33 +0000

    added comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,12 +3,12 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const createTweetElement = function(tweetObj) {
+const createTweetElement = function(tweetObj) { //creates each individual tweet
   const avatar = tweetObj.user.avatars;
   const name = tweetObj.user.name;
   const handle = tweetObj.user.handle;
   const content = tweetObj.content.text;
-  const timeAgo = timeago.format(tweetObj.created_at);
+  const timeAgo = timeago.format(tweetObj.created_at); //uses timeago to convert time code to timeago format
 
   const $tweet = $(`<article class="tweet-container">
     <div class = "name-header">  
@@ -28,45 +28,50 @@ const createTweetElement = function(tweetObj) {
       </div>
     </footer> 
   </article>`);
+
+  //adds HTML article for each tweet
+
   return $tweet;
 };
 
 const renderTweets = function(tweets) {
   for (let tweet of tweets) {
-    $('.all-tweets').prepend(createTweetElement(tweet));
+    $('.all-tweets').prepend(createTweetElement(tweet)); //appends each new tweet to the top of the "all tweets" division so new tweets appear first
   }
 };
 
 const escape = function(str) {
-  let div = document.createElement("div");
+  let div = document.createElement("div"); //escape function to avoid XSS errors
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
 
 const loadTweets = function() {
-  $.get("/tweets", function(tweets) {
+  $.get("/tweets", function(tweets) { //function gets tweets from /tweets json, deletes all previously loaded tweets and runs them through the rendersTweet function,
     $('.all-tweets').empty();
     renderTweets(tweets);
   });
 };
-const slideUpAllError = function() {
+const slideUpAllError = function() { //collects slide up functions for all error messages
   $(".error-messages").slideUp();
   $(".chara-error").slideUp();
   $(".null-error").slideUp();
 };
 
 const slideDownLengthError = function() {
+  //collects slide down functions for length error
   $(".error-messages").slideDown();
   $(".chara-error").slideDown();
 };
 
 const slideDownEmptyError = function() {
+  //collects slide down functions for empty string error
   $(".error-messages").slideDown();
   $(".null-error").slideDown();
 };
 
-const submitTweets = function() {
+const submitTweets = function() { //submits tweets from the form to the /tweets json, resets the counter and reloads all the tweets
   const submitData = $('#tweet-text').serialize();
   $.post("/tweets", submitData,  function(event) {
     $('.textarea').val('');
@@ -81,18 +86,18 @@ $(document).ready(function() {
   
   $(".tweet-function").submit(function(event) {
     
-    event.preventDefault();
+    event.preventDefault(); //prevents default event from occuring when "tweet" button is clicked
     slideUpAllError();
 
     const valData = $('.textarea').val();
   
-    if (valData.length > 140) {
+    if (valData.length > 140) { //if length of tweet exceeds 140, returns error
       slideDownLengthError();
       return;
 
     }
     
-    if (valData === "") {
+    if (valData === "") { //if string is empty returns error,
       slideDownEmptyError();
       return;
     }
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-  $("textarea").on('input', function() {
-    const charaCount = 140 - $(this).val().length;
+  $("textarea").on('input', function() { //takes input data from form on page
+    const charaCount = 140 - $(this).val().length; //sets counter value to equal 140 - # of characters inputed
     const counter = $(this).parent().find('.counter');
-    counter[0].innerText = charaCount;
+    counter[0].innerText = charaCount; //replaces innerText with counter
     if (charaCount < 0) {
-      counter.css("color", "red");
+      counter.css("color", "red"); //if counter fallse below zero colour changes to red
     }
     if (charaCount >= 0) {
       counter.css("color", "rgba(65, 60, 60, 0.836)");
+      //if counter returns to zero or above colour returns to normal
     }
 
   });
